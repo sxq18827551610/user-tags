9b9e1781f63a8e56f5b0ca20c85ae67a28956e94
confucianzuoyuan <zuoyuantc@126.com>
2020-03-07 20:30:58 +0800

    update

---
+++ usertags-project/src/main/java/com/atguigu/bigdata/sparkestag/support/EsTag.java
@@ -0,0 +1,10 @@
+package com.atguigu.bigdata.sparkestag.support;
+
+import lombok.Data;
+
+@Data
+public class EsTag {
+    private String name;
+    private String value;
+    private String type;
+}
---
+++ usertags-project/src/main/java/com/atguigu/bigdata/sparkestag/web/CountController.java
@@ -0,0 +1,19 @@
+package com.atguigu.bigdata.sparkestag.web;
+
+import com.atguigu.bigdata.sparkestag.support.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountController {
+
+    @Autowired
+    RedisUtil redisUtil;
+
+    @GetMapping("/count")
+    public String count() {
+        return "Now , the server's count is " +
+                redisUtil.incr("count", 1L);
+    }
+}
